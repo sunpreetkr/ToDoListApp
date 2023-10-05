326c019020484bd23623d2a4020e36ad331ce84e
sunpreetkr <sunpreetkaur0403@gmail.com>
2023-10-05 23:14:05 +1100

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Sunpreet Kaur on 5/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
